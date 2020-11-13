2f816b8e861873aec1b28190e8c55726e6597f80
Caint-dev <Caint-dev@users.noreply.github.com>
2020-11-13 17:09:51 +0100

    Update controller.php

---
+++ controller/controller.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+/*
+Fonction pour générer un code de partiealéatoire
+
+*/ 
+
+function generateRandomString($length = 6) {
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
