6985eae0df92b1925210359bed55e985f620799f
Mathieu RODRIGUES <74418584+MathieuRodri@users.noreply.github.com>
2020-11-15 20:16:02 +0100

    Bug fix - Problème d'affichage sur grands écrans.

--- view/frontend/homeView.php
+++ view/frontend/homeView.php
@@ -2,23 +2,18 @@
 <?php ob_start();?>
 
 <div id = "container2">
+  <input  type="text" name="pseudo" id="pseudo"  class ="element" placeholder="PSEUDO" size="300" maxlength="10" />
 
-      
-           
-            <input  type="text" name="pseudo" id="pseudo"  class ="element" placeholder="PSEUDO" size="300" maxlength="10" />
-  
-    <a class = "block create element" href = "index.php?action=create"> 
-        <p>Créer</p>
+  <div class="blocks">
+    <a class = "block create element" href = "index.php?action=create">
+      <p>Créer</p>
     </a>
 
-    <a class = "block join element" href = "index.php?action=join"   > 
-        <p>Rejoindre</p>
+    <a class = "block join element" href = "index.php?action=join"   >
+      <p>Rejoindre</p>
     </a>
-    
+  </div>
 </div>
-
-
 
 <?php $content = ob_get_clean(); ?>
 <?php require('template.php'); ?>uj
-
