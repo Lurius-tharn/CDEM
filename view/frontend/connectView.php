d7f1fd76450a5f995dc962e78662bffb5dd94732
Clement3010 <73697560+Clement3010@users.noreply.github.com>
2020-11-20 16:48:42 +0100

    Add files via upload

--- view/frontend/connectView.php
+++ view/frontend/connectView.php
@@ -1,40 +1,54 @@
 <?php $title = "Se connecter" ?>
 <?php ob_start(); ?>
 
-<div class=container style="text-align:center">
 
-    <div class="block" style="padding:10em" style="margin:1em">
-        <?php
-        if (isset($_GET['incorrect'])) {
-            if ($_GET['incorrect'] == 1) {
-                echo "<span class=err>Identifiant non reconnu.</span>";
-            } else {
-                echo "<span class=err>Mot de passe incorrect.</span>";
-            }
-        }
-        ?>
+<div class="connectBlocks">
+    <div class="connectblock">
+        <div class="connectForm">
+            <h1>Déjà membre ?</h1>
+            <div class="inputBlocks">
+                <div class="inputBlock">
+                    <h2>Pseudo</h2>
+                    <input type="text" name="pseudo" id="pseudoCon" class="element" placeholder="Votre Pseudo" maxlength="30" />
+                </div>
+                <div class="inputBlock">
+                    <h2>Mot de passe</h2>
+                    <input type="text" name="pwd" id="pwdCon" class="element" placeholder="8 caractères min." maxlength="255" />
+                    <a href="">Mot de passe oublié ?</a>
+                </div>
+            </div>
+        </div>
 
-        <form action="co.php" method="post">
-            <br>
-            Entrez votre identifiant (email):
-            <br>
-            <input type="text" name=pseudo>
+        <button class="button" type="button" name="buttonCon">
+            <p>Se connecter</p>
+        </button>
+    </div>
 
-            <br>
-            Entrez votre mot de passe:
-            <br>
-            <input type="password" name=mdp>
-            <br>
-            <input type=submit>
-        </form>
+    <div class="connectblock">
+        <div class="connectForm">
+            <h1>Pas encore membre ?</h1>
+            <div class="inputBlocks">
+                <div class="inputBlock">
+                    <h2>Email</h2>
+                    <input type="text" name="email" id="emailIns" class="element" placeholder="Votre Email" maxlength="255" />
+                </div>
+                <div class="inputBlock">
+                    <h2>Pseudo</h2>
+                    <input type="text" name="pseudo" id="pseudoIns" class="element" placeholder="Votre Pseudo" maxlength="255" />
+                </div>
+                <div class="inputBlock">
+                    <h2>Mot de passe</h2>
+                    <input type="text" name="pwd" id="pwdIns" class="element" placeholder="8 caractères min." maxlength="10" />
+                </div>
+            </div>
+        </div>
 
-        <br>
-        Pas encore de compte ?
-        <br>
-
-        <a>créer un compte !</a>
+        <button class="button" type="button" name="buttonIns">
+            <p>S'inscrire</p>
+        </button>
     </div>
 </div>
 
 <?php $content = ob_get_clean(); ?>
+<?php $css = "<link href=\"public/css/connexion.css?v=<?php echo time(); ?>\" rel=\"stylesheet\" />" ?>
 <?php require('template.php'); ?>
