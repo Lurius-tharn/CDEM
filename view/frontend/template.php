d916bafd532a53efe50bd3b80db860199aef9d14
Caint-dev <Caint-dev@users.noreply.github.com>
2020-11-20 12:39:26 +0100

    update joinView.php and add create view
    Add the 2 pages, have make it responsive, javascript for scrollbar is bugged, in a wait of solving

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -3,9 +3,11 @@
 
 <head>
     <meta charset="utf-8" />
+    
     <title><?= $title ?></title>
     <link href="public/css/style.css?v=<?php echo time(); ?>" rel="stylesheet" />
     <?php if (isset($css)) {echo $css;} ?>
+
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap" rel="stylesheet">
 </head>
@@ -29,5 +31,5 @@
 
     </footer>
 </body>
-
+<?php if (isset($js)) {echo $js;} ?>
 </html>
