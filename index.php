ac1c65c71137b5830db7939201a58a22f821457e
Clement3010 <73697560+Clement3010@users.noreply.github.com>
2020-11-28 19:41:33 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -5,13 +5,17 @@ require('controller/controller.php');
 if (isset($_GET['action'])) {
     if ($_GET['action'] == 'connect') {
         connectView();
+    } else if ($_GET['action'] == 'disconnect') {
+        $PlayerManager = new PlayerManager();
+        $PlayerManager->disconnectPlayer();
+        homeView();
     } else if ($_GET['action'] == 'check') {
         checkLogin();
-    }else if ($_GET['action'] == 'new') {
+    } else if ($_GET['action'] == 'new') {
         newLogin();
-    }else if ($_GET['action'] == 'forgottenPwd') {
+    } else if ($_GET['action'] == 'forgottenPwd') {
         forgottenPwd();
-    }else if (!isset($_COOKIE['pseudo']) OR empty($_COOKIE['pseudo'])) {
+    } else if (!isset($_COOKIE['username']) or empty($_COOKIE['username'])) {
         homeView();
     } else if ($_GET['action'] == 'create') {
         createView();
