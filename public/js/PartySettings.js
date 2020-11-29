9b836d7a7fb684e1e667c411220eda945cdce7f0
Clement3010 <73697560+Clement3010@users.noreply.github.com>
2020-11-29 17:33:37 +0100

    Add files via upload

--- public/js/PartySettings.js
+++ public/js/PartySettings.js
@@ -1,43 +1,48 @@
 
-function buttonClicked(ClickedButton)
-{
+function buttonClicked(ClickedButton) {
     var privateButton = document.getElementById("Privé");
     var publicButton = document.getElementById("Public");
     var boolPArty = document.getElementById("hiddenBool");
-    if (ClickedButton== "Privé") {
+    if (ClickedButton == "Privé") {
         privateButton.classList.remove("notClick");
         publicButton.classList.add("notClick");
-        
-    }if (ClickedButton == "Public" ) {
+    } if (ClickedButton == "Public") {
         privateButton.classList.add("notClick");
-     publicButton.classList.remove("notClick");
+        publicButton.classList.remove("notClick");
     }
-    boolPArty.value  =ClickedButton;
+    boolPArty.value = ClickedButton;
 }
 
 
 
-function buttonChange( button) 
-{
+function buttonChange(button, page) {
     var privateBlock = document.getElementById("private");
     var publicBlock = document.getElementById("public");
-   
+
     if (button == 'private') {
-       
         privateBlock.classList.remove("hidden");
         publicBlock.classList.add("hidden");
         buttonClicked("Privé");
-        
+        if (page == 'create') {
+            document.getElementById("hiddenBool").value = "Privé";
+        }
+        if (page == 'join') {
+            document.getElementById("codeJoin").required = true;
+        }
     }
     if (button == 'public') {
         publicBlock.classList.remove("hidden");
         privateBlock.classList.add("hidden");
         buttonClicked("Public");
- 
-        
-        
-
+        if (page == 'create') {
+            document.getElementById("hiddenBool").value = "Public";
+        }
+        if (page == 'join') {
+            document.getElementById("codeJoin").required = false;
+        }
     }
+
+
 }
 
 
@@ -45,24 +50,24 @@ function ScrollValue(Scroll, Value) {
     var slider = document.getElementById(Scroll);
     var output = document.getElementById(Value);
     output.innerHTML = slider.value;
-    slider.onInput = function() {
+    slider.onInput = function () {
         output.innerHTML = this.value;
-      } 
+    }
 
 }
 
 function PseudoValidator(liens) {
-    var pseudo =document.getElementById('pseudo');
-    if(pseudo.value == null){
-        pseudo.style.backgroundColor ='red';
-    }else{
-        var link =this.href='index.php?action='+liens+'&pseudo='+pseudo.value;
+    var pseudo = document.getElementById('pseudo');
+    if (pseudo.value == null) {
+        pseudo.style.backgroundColor = 'red';
+    } else {
+        var link = this.href = 'index.php?action=' + liens + '&pseudo=' + pseudo.value;
         return link;
     }
-    
+
 }
 
 
-window.addEventListener("load",function(){
+window.addEventListener("load", function () {
     buttonClicked('Privé');
 });
